5909faaf1e66d785afaf50ebec93ec6e85c5e94a
NAGAVENIGOWDA <nagavenigowda017@gmail.com>
2023-08-21 14:46:06 +0530

    initial commit

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,24 +1,91 @@
-import "./portfolio.scss"
+import PortfolioList from "../PortfolioList/PortfolioList";
+import "./portfolio.scss";
+import { useState, useEffect } from "react";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+        default:
+          setData(featuredPortfolio);
+    }
+  }, [selected]);
   return (
     <div className="portfolio" id="portfolio">
-     <h1>Portfolio</h1>
-     <ul>
-      <li className="active">Featured</li>
-      <li>Web App</li>
-      <li>Mobile App</li>
-      <li>Design</li>
-      <li>Branding</li>
-     </ul>
-     <div className="conatiner">
-      <div className="item">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => {
+          return (
+            <PortfolioList
+              title={item.title}
+              active={selected === item.id}
+              setSelected={setSelected}
+              id={item.id}
+            />
+          );
+        })}
+      </ul>
+      <div className="container">
+        {data.map(item=>{
+          return(
+            <div className="item">
+            <img src={item.img} alt="" />
+            <h3>{item.title}</h3>
+             
+           </div>
+          )
+            
+        })}
+       
       </div>
-     </div>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
