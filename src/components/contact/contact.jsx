61438f794e7468ee6d427ab0c90a7f93cb5adc15
NAGAVENIGOWDA <nagavenigowda017@gmail.com>
2023-08-25 21:18:03 +0530

    initial commit

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,26 @@
 
+import { useState } from "react"
 import "./contact.scss"
 function Contact() {
+const [msg,setMsg]=useState(false)
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    setMsg(true)
+  }
   return (
     <div className="contact" id="contact">
-      contact
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="Email"/>
+          <textarea  placeholder="Message"></textarea>
+          <button type="Submit">Send</button>
+          {msg && <span>Thanks,I'll reply ASAP :)</span>}
+        </form>
+      </div>
     </div>
   )
 }
